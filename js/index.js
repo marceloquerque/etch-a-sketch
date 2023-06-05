447910a00bfe4af2dbcf975fe31728e5ceecfa11
Marcelo Querque <querquemarcelo@gmail.com>
2023-06-05 09:20:20 -0300

    Start making grid with js

--- js/index.js
+++ js/index.js
@@ -1,2 +1,11 @@
-document.querySelector(".inner-frame")
+// create a new <div> element
+for (let i = 1; i < 166; i++) {
+    let grid = document.createElement("div");
+    grid.classList.add("grid")
+    const gridFrame = document.querySelector(".inner-frame")
+    gridFrame.appendChild(grid)
+}
 
+// add a css class to the <div>
+
+// append the <div> to the parent

97747614959df4863f3c7cea992f5d6a9626cac9
Marcelo Querque <querquemarcelo@gmail.com>
2023-06-05 12:28:11 -0300

    Very difficult wow

--- js/index.js
+++ js/index.js
@@ -2,6 +2,19 @@ let numberOfColumns = 16;
 let numberOfRows = 12;
 let gridSize = 56.25;
 
+const colors = {
+    black: "#000",
+    ice: ["#E8F9FD", "#79DAE8", "#0AA1DD", "#2155CD"],
+    sunset: ["#FFE15D", "#F49D1A", "#DC3535", "#B01E68"],
+}
+
+function randColor() {
+    let rand = colors.ice[Math.floor(Math.random() * colors.ice.length)]
+    console.log(rand)
+}
+
+randColor()
+
 function createGrid() {
     let grid = document.createElement("div");
     grid.classList.add("grid")

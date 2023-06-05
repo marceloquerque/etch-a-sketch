0ac3e6dd8e4abba5aca4baa6847aa6605d6dcfb6
Marcelo Querque <querquemarcelo@gmail.com>
2023-06-05 11:59:03 -0300

    Start adding controls

--- js/index.js
+++ js/index.js
@@ -1,9 +1,19 @@
-// create a new <div> element
-for (let i = 1; i < 193; i++) {
+let numberOfColumns = 16;
+let numberOfRows = 12;
+let gridSize = 56.25;
+
+function createGrid() {
     let grid = document.createElement("div");
     grid.classList.add("grid")
-    const gridFrame = document.querySelector(".inner-frame")
-    gridFrame.appendChild(grid)
+    const gridFrame = document.querySelector(".grid-frame")
+    return gridFrame.appendChild(grid)
+}
+
+for (let c = 0; c < numberOfColumns; c++){
+    createGrid()
+    for (let r = 1; r < numberOfRows; r++){
+        createGrid()
+    }
 }
 
 const grids = document.querySelectorAll(".grid");

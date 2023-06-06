480d6a20b9534d2064471d34e19090eb0bdc0781
Marcelo Querque <querquemarcelo@gmail.com>
2023-06-06 18:12:39 -0300

    Need to fix the colors button and random color function

--- js/index.js
+++ js/index.js
@@ -1,38 +1,73 @@
-let numberOfColumns = 16;
-let numberOfRows = 12;
-let gridSize = 56.25;
+// grid dimension options
+const dimensions = {
+    mini: { columns: 8, rows: 6, size: 112.5 },
+    small: { columns: 16, rows: 12, size: 56.25 },
+    medium: { columns: 32, rows: 24, size: 28.125 },
+    big: { columns: 64, rows: 48, size: 14.0625 },
+    large: { columns: 128, rows: 92, size: 7.01335 },
+  };
 
+// color options
 const colors = {
-    black: "#000",
+    black: ["#C0C0C0", "#343434", "#000000"],
     ice: ["#E8F9FD", "#79DAE8", "#0AA1DD", "#2155CD"],
     sunset: ["#FFE15D", "#F49D1A", "#DC3535", "#B01E68"],
 }
 
-function randColor() {
-    let rand = colors.ice[Math.floor(Math.random() * colors.ice.length)]
-    console.log(rand)
-}
+const gridFrame = document.querySelector(".grid-frame")
 
-randColor()
+// create <div>'s and append them to the parent
+function createGrid(numberOfColumns, numberOfRows, size) {
+    
+    // clear the existing grid
+    gridFrame.innerHTML = "" 
 
-function createGrid() {
-    let grid = document.createElement("div");
-    grid.classList.add("grid")
-    const gridFrame = document.querySelector(".grid-frame")
-    return gridFrame.appendChild(grid)
-}
-
-for (let c = 0; c < numberOfColumns; c++){
-    createGrid()
-    for (let r = 1; r < numberOfRows; r++){
-        createGrid()
+    for (let c = 0; c < numberOfColumns; c++) {
+        for (let r = 0; r < numberOfRows; r++) {
+            let grid = document.createElement("div");
+            grid.classList.add("grid")
+            grid.setAttribute("style", `width: ${size}px; height: ${size}px`)
+            gridFrame.appendChild(grid)
+            grid.addEventListener("mouseover", (event) => {
+                event.target.style.background = randColor()
+            })
+        }
     }
 }
 
-const grids = document.querySelectorAll(".grid");
+const buttons = document.querySelectorAll(".btn")
 
-Array.from(grids).forEach(grid => {
-    grid.addEventListener("mouseover", (event) => {
-        event.target.style.background = "purple"
-    })
-})
+// change grid size buttons functionality 
+buttons.forEach(button => {
+  button.addEventListener("click", (e) => {
+    const buttonId = e.target.id;
+    const newGridSize = dimensions[buttonId];
+    if (newGridSize) {
+      const { columns, rows, size } = newGridSize;
+      createGrid(columns, rows, size);
+    }
+
+    if (buttonId in colors) {
+        let selectedColor = buttonId
+        randColor(selectedColor)
+    }
+
+  });
+});
+
+// random colors
+function randColor(color) {
+    console.log(typeof color)
+    console.log(typeof sunset)
+    return colors.sunset[Math.floor(Math.random() * colors.sunset.length)]
+}
+
+// start grid size values
+function startSize() {
+    const { columns, rows, size } = dimensions["small"]
+    createGrid(columns, rows, size)
+}
+
+startSize()
+
+
